8cc46775f1e78b9385a03b72be9460b05b290157
zcjava <zcjava@qq.com>
2025-09-22 00:18:36 +0800

    add starship

--- Mackup/.config/fish/config.fish
+++ Mackup/.config/fish/config.fish
@@ -2,6 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     bass source ~/.bash_profile
 
+    starship init fish | source
+
     alias ls "ls -G"
 
     # use ctrl+c copy seleted search history 
