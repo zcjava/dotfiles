8a62247d00264cf2264db0f14be04b1dd2bf1ddb
zcjava <zcjava@qq.com>
2025-09-22 23:55:14 +0800

    add fish plugin sponge

--- Mackup/.config/fish/config.fish
+++ Mackup/.config/fish/config.fish
@@ -7,5 +7,10 @@ if status is-interactive
     alias ls "ls -G"
 
     # use ctrl+c copy seleted search history 
-    set fzf_history_opts --bind "ctrl-c:execute-silent(printf '%s\n' {+} | string replace --all --regex '^.*? │ ' '' | fish_clipboard_copy)+abort"
+    set -g fzf_history_opts --bind "ctrl-c:execute-silent(printf '%s\n' {+} | string replace --all --regex '^.*? │ ' '' | fish_clipboard_copy)+abort"
+    # sponge setting
+    set -g sponge_purge_only_on_exit true
+    set -g sponge_successful_exit_codes 0
+    set -g sponge_allow_previously_successful false
+
 end
